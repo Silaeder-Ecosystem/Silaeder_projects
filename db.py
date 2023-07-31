dae914b6ccea47aa86495214f4eff1c6e48472d7
ilyastarcek <ilyastar9999>
2023-07-31 18:16:56 +0300

    fix main page

--- db.py
+++ db.py
@@ -1,16 +1,19 @@
 import sqlite3
 
-sqlite_connection = sqlite3.connect('db.db')
+sqlite_connection = sqlite3.connect('db.sql', check_same_thread=False)
 cursor = sqlite_connection.cursor()
 
 def create_all():
-    sqlite_select_query = """CREATE TABLE IF NOT EXISTS projects(id SEREAL, autor_usernames TEXT, name TEXT, descrip TEXT, dir_with_pic TEXT, video_link TEXT, topic TEXT, teamlead TEXT);
-CREATE TABLE IF NOT EXISTS users(id SEREAL, username TEXT UNIQUE, name TEXT, surname TEXT);"""
-    cursor.execute(sqlite_select_query)
+    sqlite_select_query = ["""CREATE TABLE IF NOT EXISTS projects(id SEREAL, autor_usernames TEXT, title TEXT, descrip TEXT, dir_with_pic TEXT, video_link TEXT, topic TEXT, teamlead TEXT);""", 
+"""CREATE TABLE IF NOT EXISTS users(id SEREAL, username TEXT UNIQUE, name TEXT, surname TEXT);"""]
+    cursor.execute(sqlite_select_query[0])
+    cursor.execute(sqlite_select_query[1])
     return
 
 def get_all_projects():
     sqlite_select_query = """SELECT title, teamlead, topic FROM projects"""
     cursor.execute(sqlite_select_query)
     return cursor.fetchall()
+
+
 
--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import db as db
 
 app = Flask(__name__)
 
-@app.route('/', metods='POST')
+db.create_all()
+
+@app.route('/', methods=['GET'])
 def main():
-    db.create_all()
     ans = db.get_all_projects()
     return render_template("index.html", projects = ans)
 
