65371da3fc7bda448c139d186319aa01312bf534
ilyastarcek <ilya.starchak@yandex.com>
2023-08-31 15:14:43 -0400

    verion 0.9(becose frontend is broken)

---
+++ db.py
@@ -0,0 +1,225 @@
+import psycopg2
+import json
+
+config_file = open("config.json")
+config_data = json.load(config_file)
+host = config_data["db_host"]
+port = config_data["db_port"]
+user = config_data["db_user"]
+database = config_data["db_name"]
+password = config_data["db_pass"]
+
+conn = psycopg2.connect(
+    host=host,
+    port=port,
+    database=database,
+    user=user,
+    password=password
+)
+
+conn.autocommit = True
+
+cursor = conn.cursor()
+
+conn.rollback()
+
+def parse_data(field):
+    file = open("config.json")
+    data = json.load(file)[field]
+
+    return data
+
+def get_all_projects():
+    sqlite_select_query = """SELECT title, teamlead, topic, id, main_pic_path FROM projects;"""
+    cursor.execute(sqlite_select_query)
+    conn.commit()
+    return cursor.fetchall()
+
+def get_user_by_username(username):
+    sqlite_select_query = """SELECT name, surname FROM users WHERE username = %s;"""
+    cursor.execute(sqlite_select_query, username)
+    conn.commit()
+    return cursor.fetchall()
+
+def get_user_by_id(id):
+    sqlite_select_query = """SELECT username, name, surname FROM users WHERE id = %s AND auth = true;"""
+    cursor.execute(sqlite_select_query, id)
+    conn.commit()
+    return cursor.fetchall()
+
+def create_user(username, email, password, name, surname):
+    #try:
+        sqlite3_insert_query = """INSERT INTO users (username, email, password, name, surname, auth) VALUES (%s, %s, %s, %s, %s, false);"""
+        cursor.execute(sqlite3_insert_query, (username, email, password, name, surname, ))
+        conn.commit()
+        return True
+    #except:
+        return False
+
+def auth_user(username):
+    sqlite3_select_query = """Update users SET auth = true WHERE username = %s;"""
+    cursor.execute(sqlite3_select_query, (username, ))
+    return
+        
+def check_auth_user(username):
+    sqlite3_select_query = """SELECT auth FROM users WHERE username = %s;"""
+    cursor.execute(sqlite3_select_query, (username, ))
+    conn.commit()
+    try:
+        if cursor.fetchall()[0][0]:
+            return True
+        else:
+            return False
+    except:
+        return True
+
+def get_email_by_username(username):
+    sqlite3_select_query = """SELECT email FROM users WHERE username = %s;"""
+    cursor.execute(sqlite3_select_query, (username, ))
+    conn.commit()
+    return cursor.fetchall()
+
+def get_is_user_logged_in(username, password):
+    sqlite3_select_query = """SELECT auth FROM users WHERE username =%s AND password =%s;"""
+    cursor.execute(sqlite3_select_query, (username, password, ))
+    conn.commit()
+    ans = cursor.fetchall()
+    if ans != []:
+        return ans[0]
+    else:
+        return False
+    
+def check_user_is_exist(username):
+    sqlite3_select_query = """SELECT username FROM users WHERE username =%s AND auth = true;"""
+    cursor.execute(sqlite3_select_query, (username, ))
+    conn.commit()
+    if cursor.fetchall() == []:
+        return False
+    else:
+        return True
+    
+def check_not_auth_user_is_exist(username):
+    sqlite3_select_query = """SELECT username FROM users WHERE username =%s;"""
+    cursor.execute(sqlite3_select_query, (username, ))
+    conn.commit()
+    return cursor.fetchall()
+
+def create_project(title, descrip, teamlead, autor_usernames, video_link, dir_with_pic, topic, main_pic_path, links, pdf_link):
+    sqlite3_select_query = """INSERT INTO projects (title, descrip, teamlead, autor_usernames, video_link, dir_with_pic, topic, main_pic_path, links, pdf_link) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s, %s);"""
+    print(sqlite3_select_query)
+    cursor.execute(sqlite3_select_query, (title, descrip, teamlead, autor_usernames, video_link, dir_with_pic, topic, main_pic_path, links, pdf_link))
+    conn.commit()
+    return True
+    
+def get_project_by_id(id):
+    sqlite3_select_query = """SELECT * FROM projects WHERE id =%s;"""
+    cursor.execute(sqlite3_select_query, (id, ))
+    conn.commit()
+    return cursor.fetchall()
+
+def update_project(id, title, descrip, teamlead, autor_usernames, video_link, dir_with_pic, topic, main_pic_path, links, pdf_link):
+    if 1==1:
+        sqlite3_update_query = """UPDATE projects SET title =%s, descrip =%s, teamlead =%s, autor_usernames = %s, video_link =%s, dir_with_pic =%s, topic =%s, main_pic_path =%s, links =%s, pdf_link=%s WHERE id =%s;"""
+        cursor.execute(sqlite3_update_query, (title, descrip, teamlead, autor_usernames, video_link, dir_with_pic, topic, main_pic_path, links, pdf_link, id, ))
+        conn.commit()
+        return True
+    
+def get_all_usernames():
+    sqlite3_select_query = """SELECT username FROM users;"""
+    cursor.execute(sqlite3_select_query)
+    conn.commit()
+    return cursor.fetchall()
+
+def get_user_id_by_username(username):
+    sqlite3_select_query = """SELECT id FROM users WHERE username =%s;"""
+    cursor.execute(sqlite3_select_query, (username, ))
+    conn.commit()
+    return cursor.fetchall()
+
+def get_projects_by_username(username):
+    query= 'SELECT title, teamlead, topic, id, main_pic_path FROM projects WHERE %s = ANY(autor_usernames);'
+    cursor.execute(query, (username, ))
+    conn.commit()
+    return cursor.fetchall()
+
+def delete_project(id):
+    try:
+        sqlite3_delete_query = """DELETE FROM projects WHERE id =%s;"""
+        cursor.execute(sqlite3_delete_query, (id, ))
+        conn.commit()
+        return True
+    except:
+        return False
+    
+def delete_user(id):
+    try:
+        sqlite3_delete_query = """DELETE FROM users WHERE id =%s;"""
+        cursor.execute(sqlite3_delete_query, (id, ))
+        conn.commit()
+        return True
+    except:
+        return False
+    
+def delete_all():
+    try:
+        sqlite3_delete_query = """DROP TABLE projects, users;"""
+        cursor.execute(sqlite3_delete_query)
+        conn.commit()
+        return True
+    except:
+        return False
+    
+def is_user_in_project(id, username):
+    query= 'SELECT title, teamlead, topic FROM projects WHERE %s IN projects.autor_usernames;'
+    cursor.execute(query, (username, id, ))
+    conn.commit()
+    if cursor.fetchall() != []:
+        return True
+    else:
+        return False
+    
+def is_user_teamlead(id, username):
+    sqlite3_select_query = """SELECT teamlead FROM projects WHERE id =%s;"""
+    cursor.execute(sqlite3_select_query, (id, ))
+    conn.commit()
+    if cursor.fetchall()[0][0] == username:
+        return True
+    else:
+        return False
+
+def create_all():
+    sqlite_select_query = ["""CREATE TABLE IF NOT EXISTS projects(id SERIAL PRIMARY KEY, autor_usernames TEXT ARRAY, title TEXT, descrip TEXT, dir_with_pic TEXT, video_link TEXT, topic TEXT, teamlead TEXT, main_pic_path TEXT, links TEXT, pdf_link TEXT);""", 
+"""CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, username TEXT UNIQUE, name TEXT, surname TEXT, password TEXT, auth BOOL, email TEXT UNIQUE);"""]
+    cursor.execute(sqlite_select_query[0])
+    cursor.execute(sqlite_select_query[1])
+    conn.commit() 
+    try:
+        create_user("admin", "silaederprojects@gmail.com", parse_data("secret_key"), "Admin", "Adminovich")
+    except:
+        pass
+    return
+
+def count_of_projects():
+    query= 'SELECT MAX(id) FROM projects'
+    cursor.execute(query)
+    conn.commit()
+    return cursor.fetchall()[0][0]
+
+def search_for_projects(title):
+    query = """
+    SELECT *
+    FROM (
+        SELECT title, teamlead, topic, id, main_pic_path, similarity(LOWER(title), %s) AS s_title
+        FROM projects
+    ) AS subquery
+    WHERE LOWER(title) LIKE %s AND s_title > 0.1
+    ORDER BY s_title DESC;
+"""
+    cursor.execute(query, (title.lower(), '%' + title.lower() + '%'))
+
+    conn.commit()
+    ans = cursor.fetchall()
+    for i in range(len(ans)):
+        ans[i] = list(ans[i])
+        del ans[i][-1]
+    return ans
---
+++ main.py
@@ -0,0 +1,338 @@
+from flask import Flask, request, render_template, redirect, make_response, flash
+import db as db
+import jwt
+import json
+import parse
+from flask_mail import Message, Mail
+import re
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+#db.delete_all()
+
+db.create_all()
+
+#db.create_project("Silaeder Projects", "site_for_Silaeder_projects", "ilyastarcek", ['ilyastarcek', 'NICITATURBOBOY'], "нет", "нет", "IST", 'icon.jpg', 'projects.sileder.ru', 'нет')
+
+def parse_data(field):
+    file = open("config.json")
+    data = json.load(file)[field]
+
+    return data
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'silaederprojects@gmail.com'  
+app.config['MAIL_DEFAULT_SENDER'] = 'silaederprojects@gmail.com'  
+app.config['MAIL_PASSWORD'] = parse_data("mail_password")
+app.config['UPLOAD_FOLDER'] = './static/'
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+mail_sender = Mail(app)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def send_email(to, subject, template):
+    msg = Message(
+        subject,
+        recipients=[to],
+        html=template,
+        sender=parse_data('mail')
+    )
+    mail_sender.send(msg)
+
+def generate_confirmation_token(email):
+    return jwt.encode(payload={"name": email}, key=parse_data("secret_key"))
+
+
+def confirm_token(token):
+    try:
+    	return jwt.decode(token, key=parse_data("secret_key"), algorithms="HS256")["name"]
+    except:
+    	return False
+
+
+def check_jwt(token, username):
+    try:
+        payload = jwt.decode(token, key=parse_data("secret_key"), algorithms="HS256")
+    except:
+        return False
+        
+    if payload["name"] == username:
+        return True
+    else:
+        return False
+
+@app.route('/', methods=['GET'])
+def main():
+    if not check_jwt(request.cookies.get("jwt"), "admin"):
+        return redirect('/projects')
+    return "HELLO"
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    
+    else:
+        login = request.form["login"]
+        password = request.form["password"]
+
+        if db.get_is_user_logged_in(login, password):
+            token = jwt.encode(payload={"name": login}, key=parse_data("secret_key"))
+            resp = make_response(redirect("/"))
+            resp.set_cookie("jwt", token)
+            return resp
+        else:
+            flash('Wrong login or password')
+            return redirect("/login", code=302)
+
+
+@app.route('/registration', methods=['GET', 'POST'])
+def register():
+    if (request.method == "GET"):
+        return render_template("reg.html")
+    else:
+        form = request.form.to_dict()
+        form['email'] = form['email'].lower()
+        hasDigits, hasUpperCase, hasLowerCase, hasSpecialCharecters, hasSpases = False, False, False, False, True
+
+        for i in form["password"]:
+            if (i.isdigit()):
+                hasDigits = True
+            elif (i.isupper()):
+                hasUpperCase = True
+            elif (i.islower()):
+                hasLowerCase = True
+            elif (i == ' '):
+                hasSpases = False
+            else:
+                hasSpecialCharecters = True 
+
+        if ( hasDigits and hasLowerCase and hasSpases and hasUpperCase and hasSpecialCharecters and form["password"] == form["password2"]):
+            if not re.match(r"[^@]+@[^@]+\.[^@]+", form["email"]):
+                flash('You write incorrect email')
+                return redirect("/registration", code=302)
+            app.logger.debug(parse.parse_csv())
+            try:
+                if parse.parse_csv().index(form["email"]) == -1:
+                    flash('This is not silaeder email. Check it in Silaeder google sheet or ask administrator (@ilyastarcek)')
+                    return redirect("/registration", code=302)
+            except:
+                flash('This is not silaeder email. Check it in Silaeder google sheet or ask administrator (@ilyastarcek)')
+                return redirect("/registration", code=302)
+            try:
+                if db.create_user(form['username'], form["email"], form['password'], form['name'], form['surname']) == False:
+                    flash('This username or email already exists')
+                    return redirect("/registration", code=302)
+            except:
+                flash('This username or email already exists')
+                return redirect("/registration", code=302)
+            
+            flash('A confirmation email has been sent via email')
+            resp = make_response(redirect("/", code=302))
+            
+
+            etoken = generate_confirmation_token(form["username"])
+            confirm_url = f'http://{parse_data("host")}/confirm/{etoken}'
+            html = render_template('mail.html', confirm_url=confirm_url)
+            subject = "Please confirm your email"
+            send_email(form['email'], subject, html)
+
+            return resp
+        
+        else:
+            flash('You write incorrect password')
+            return redirect("/registration", code=302)
+
+@app.route('/confirm/<token>', methods=['GET'])
+def confirm_email(token):
+    print(token)
+    try:
+        username = confirm_token(token)
+        print(username)
+    except:
+        flash('The confirmation link is invalid. Check your email')
+        return redirect('/', code=302)
+    if db.check_not_auth_user_is_exist(username) == []:
+        flash('This is link for not registered account')
+        return redirect('/registration', code=302)
+    token = jwt.encode(payload={"name": username}, key=parse_data("secret_key"))
+    if db.check_auth_user(username):
+        print(db.check_auth_user(username))
+        flash('Account already confirmed . Please login')
+        return redirect('/login', code=302)
+    else:
+        db.auth_user(username)
+        flash('You have confirmed your account. Thanks!')
+        resp = make_response(redirect("/", code=302))
+        resp.set_cookie("jwt", token)
+        return resp
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    resp = make_response(redirect("/", code=302))
+    resp.set_cookie("jwt", "")
+    return resp
+
+@app.route('/projects/new', methods=['GET', 'POST'])
+def new_projects():
+    if (request.method == "GET"):
+        if (confirm_token(request.cookies.get("jwt")) != False):
+            return render_template("new_project.html", user = confirm_token(request.cookies.get("jwt")))
+        else:
+            flash("You are not logged in")
+            return redirect('/login') 
+    else:
+        token = confirm_token(request.cookies.get("jwt"))
+        if not token:
+            flash("You are not logged in")
+            return redirect('/login') 
+        form = request.form
+        print()
+        print()
+        app.logger.debug(request.files)
+        app.logger.debug(request.form)
+        app.logger.debug(request.form.getlist('collaborators[]'))
+        print()
+        print()
+        for i in ['name', 'description', 'teacher', 'topic']:
+            if form[i] == '':
+                flash(f"You don't fill {i} field")
+                return redirect("/projects/new", code=302)
+        for i in request.form.getlist('collaborators[]'):
+            print(i)
+            if not db.check_user_is_exist(i):
+                flash(f"User {i} isn't finish registration or isn't exist. Please check him(her) username or ask him(her) finish registration")
+                return redirect("/projects/new", code=302)
+        if 'cover' not in request.files:
+            flash('No file part')
+            return redirect(f"/projects/new")
+        file = request.files['cover']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(f"/projects/new")
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_id_name = filename.rsplit('.', 1)
+            try:
+            #if 1 == 1:
+                if db.create_project(form['name'], form['description'], form['teacher'], form.getlist('collaborators[]'), form['link-video'], form['link-image'], form["topic"],  str(db.count_of_projects()+1) + '.' + file_id_name[1], form['link-interes'], form['link-pdf']) == False:
+                    flash('This project already exists')
+                    return redirect("/projects/new", code=302)
+            except:
+                flash('You fill not all fields')
+                return redirect("/projects/new", code=302)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], str(db.count_of_projects()) + '.' + file_id_name[1]))
+            flash('Project created')
+            return redirect("/myprojects", code=302)
+
+@app.route('/projects', methods=['GET'])
+def get_projects():
+    ans = db.get_all_projects()
+    print(ans)
+    return render_template("index.html", ans = ans, user = request.cookies.get("jwt"), title = "Silaeder Projects")
+
+@app.route('/projects/<id>', methods=['GET'])
+def get_project(id):
+    ans = db.get_project_by_id(id)
+    ans = list(ans[0])
+    print(ans)
+    token = confirm_token(request.cookies.get("jwt"))
+    return render_template("viewproject.html", ans = ans, user = token, id = id)
+
+@app.route('/projects/<id>/edit', methods=['GET', 'POST'])
+def edit_project(id):
+    if (request.method == 'GET'):
+        token = confirm_token(request.cookies.get("jwt"))
+        if not token:
+            flash("You are not logged in")
+            return redirect('/login') 
+        ans = db.get_project_by_id(id)
+        ans = list(ans[0])
+        print(ans)
+        return render_template("edit_project.html", ans = ans, user = token, id = id)
+    else:
+        token = confirm_token(request.cookies.get("jwt"))
+        if not token:
+            flash("You are not logged in")
+            return redirect('/login') 
+        form = request.form
+        print()
+        print()
+        app.logger.debug(request.files)
+        app.logger.debug(request.form)
+        app.logger.debug(request.form.getlist('collaborators[]'))
+        print()
+        print()
+        for i in ['name', 'description', 'teacher', 'topic']:
+            if form[i] == '':
+                flash(f"You don't fill {i} field")
+                return redirect("/projects/new", code=302)
+
+        for i in request.form.getlist('collaborators[]'):
+            print(i)
+            if not db.check_user_is_exist(i):
+                flash(f"User {i} isn't finish registration or isn't exist. Please check him(her) username or ask him(her) finish registration")
+                return redirect(f'/projects/{id}/edit', code=302)
+
+        if 'cover' not in request.files:
+            flash('No file part')
+            return redirect(f"/projects/{id}/edit")
+        file = request.files['cover']
+        if file.filename == '':
+            db.update_project(id, form['name'], form['description'], form['teacher'], form.getlist('collaborators[]'), form['link-video'], form['link-image'], form["topic"],  str(id) + '.' + file_id_name[1], form['link-interes'], form['link-pdf'])
+            flash("Project edited")
+            return redirect('/myprojects', code=200)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_id_name = filename.rsplit('.', 1)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], str(id) + '.' + file_id_name[1]))
+            db.update_project(id, form['name'], form['description'], form['teacher'], form.getlist('collaborators[]'), form['link-video'], form['link-image'], form["topic"],  str(id) + '.' + file_id_name[1], form['link-interes'], form['link-pdf'])
+            flash("Project edited")
+            return redirect('/myprojects', code=200)
+
+@app.route('/myprojects', methods=['GET'])
+def get_my_projects():
+    token = confirm_token(request.cookies.get("jwt"))
+    if not token:
+        flash("You are not logged in")
+        return redirect('/login') 
+    ans = db.get_projects_by_username(token)
+    print(ans)
+    return render_template("index.html", ans = ans, user = request.cookies.get("jwt"), title = "Projects by " + token)
+
+@app.route('/search/<inp>', methods=['GET'])
+def search(inp):
+    ans = db.search_for_projects(inp)
+    for i in range(len(ans)):
+        ans[i][4] = """{{url_for('static', filename=uploads/""" + ans[i][4] + """)}}"""
+    return render_template("index.html", ans = ans, user = request.cookies.get("jwt"), title = "Sileder Projects")
+
+@app.route('/projects/<id>/delete')
+def delete_project(id):
+    if (db.delete_project(id)):
+        for i in ALLOWED_EXTENSIONS:
+            try:
+                os.remove(f'/static/{id}.{i}')
+            except:
+                pass
+        flash('Project deleted')
+        return redirect('/projects', code=200)
+
+@app.route('/user/<username>')
+def user(username):
+    return "Nothing here (in deploment)"
+    ans = db.get_projects_by_username(username)
+    ans2 = db.get_user_by_username(username)
+    print(ans)
+    return render_template("index.html", ans = ans, user = request.cookies.get("jwt"), title = "Projects by " + username, ans2 = ans2)
+
+app.run("0.0.0.0", port=18001, debug=True)
